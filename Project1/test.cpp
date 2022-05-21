dbf27a61ec6a9faa6fcd58db64914e74e076b81c
Ubuntu Linux VM <gj.chen59@gmail.com>
2022-05-21 11:19:59 +0800

    linux version

--- Project1/test.cpp
+++ Project1/test.cpp
@@ -1,30 +1,37 @@
 #include<iostream>
+
 using namespace std;
-class Node{
+
+class Board{
+private:
 	int data[3][3];
 public:
-	int step;
-	int cost;
-	Node(){}
-	Node(int a[3][3]){
-		for (int i=0; i<3; i++)
-			for (int j=0; j<3; j++)
-				data[i][j]=a[i][j];
+	int step; // 步数
+	int cost; // 每个格子
+	Board(){}
+    Board(const int a[3][3]){
+        step = 0;
+        cost = 0;
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				data[i][j] = a[i][j];
 		cost = calc_cost();
 	}
-	int* map_rc(int i){
+/* 每个棋子的最终位置*/
+    int* map_rc(int i){
 		int* a = new int[2];
-		if (i==1){ a[0]=0; a[1]=0; }
-		else if (i==2){ a[0]=0; a[1]=1; }
-		else if (i==3){ a[0]=0; a[1]=2; }
-		else if (i==4){ a[0]=1; a[1]=0; }
-		else if (i==5){ a[0]=1; a[1]=1; }
-		else if (i==6){ a[0]=1; a[1]=2; }
-		else if (i==7){ a[0]=2; a[1]=0; }
-		else if (i==8){ a[0]=2; a[1]=1; }
+		if (i == 1){ a[0] = 0; a[1] = 0; }
+		else if (i == 2){ a[0] = 0; a[1] =1; }
+		else if (i == 3){ a[0] = 0; a[1] =2; }
+		else if (i == 4){ a[0] = 1; a[1] =0; }
+		else if (i == 5){ a[0] = 1; a[1] =1; }
+		else if (i == 6){ a[0] = 1; a[1] =2; }
+		else if (i == 7){ a[0] = 2; a[1] =0; }
+		else if (i == 8){ a[0] = 2; a[1] =1; }
 		/*else if (i==0){ a[0]=2; a[1]=2; }*/
 		return a;
 	}
+
 	int calc_cost(){
 		cost = 0;
 		for (int i=0; i<3; i++)
@@ -37,7 +44,7 @@ public:
 			}
 		return cost;
 	}
-	bool equals(const Node& node){
+	bool equals(const Board& node){
 		for (int i=0; i<3; i++)
 			for (int j=0; j<3; j++)
 				if (data[i][j]!=node.data[i][j])
@@ -59,35 +66,30 @@ public:
 		//east 0, south 1, west 2, north 3
 		for (int i=0; i<3; i++)
 			for (int j=0; j<3; j++)
-				if (dir==0){
+				if (dir == 0){
 					if (data[i][j]==0 && j!=0){
-						int temp = data[i][j];
-						data[i][j] = data[i][j-1];
-						data[i][j-1] = temp;
-						return;
+                        data[i][j] = data[i][j-1];
+                        data[i][j-1] = 0;
 					}
 				}
 				else if (dir==1){
 					if (data[i][j]==0 && i!=0){
-						int temp = data[i][j];
 						data[i][j] = data[i-1][j];
-						data[i-1][j] = temp;
+						data[i-1][j] = 0;
 						return;
 					}
 				}
 				else if (dir==2){
 					if (data[i][j]==0 && j!=2){
-						int temp = data[i][j];
 						data[i][j] = data[i][j+1];
-						data[i][j+1] = temp;
+						data[i][j+1] = 0;
 						return;
 					}
 				}
 				else{
 					if (data[i][j]==0 && i!=2){
-						int temp = data[i][j];
 						data[i][j] = data[i+1][j];
-						data[i+1][j] = temp;
+						data[i+1][j] = 0;
 						return;
 					}
 				}
@@ -102,23 +104,27 @@ public:
 					cout<<data[i][j]<<" ";
 			cout<<endl;
 		}
-		cout<<endl;
+        cout<<'cost='<<cost<<endl;
+        cout<<'step='<<step<<endl;
 	}
 };
 
-class Nodes{
-	Node data[1000];
+class Boards{
+	Board data[1000];
 	int n;
 public:
-	Nodes():n(0){}
-	void append(const Node& node){
+	Boards():n(0){}
+	void append(const Board& node){
 		data[n++] = node;
 	}
+    void sort_by_cost() {
+
+    }
 	void output(){
 		for (int i=0;i<n;i++)
 			data[i].output();
 	}
-	bool contains(const Node& node){
+	bool contains(const Board& node){
 		for (int i=0;i<n;i++)
 			if (data[i].equals(node))
 				return true;
@@ -126,14 +132,14 @@ public:
 	}
 };
 
-void main(){
+int main(){
 	int a[3][3] = {{1,2,3},{0,4,6},{7,5,8}};
 	int b[3][3] = {{1,2,3},{4,0,6},{7,5,8}};
 	int c[3][3] = {{7,1,8},{6,3,5},{0,2,4}};
-	Node node(a);
-	Node node2(b);
-	Node node3(c);
-	/*Nodes nodes;
+	Board node(a);
+	Board node2(b);
+	Board node3(c);
+	/*Boards nodes;。
 	nodes.append(node);
 	nodes.append(node2);
 	nodes.output();
